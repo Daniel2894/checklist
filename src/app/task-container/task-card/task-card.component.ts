a80ef239d57a684b1b3477508227b2b420c5b86e
Daniel Restrepo <daniel.restrepom@LATAM-G50S4M.local>
2024-04-12 20:06:42 -0500

    Task interface added

--- src/app/task-container/task-card/task-card.component.ts
+++ src/app/task-container/task-card/task-card.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from 'src/core/models/task.interface';
 
 @Component({
   selector: 'app-task-card',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./task-card.component.scss']
 })
 export class TaskCardComponent {
-
+  task: Task[] = [
+    {
+      id:1,
+      name: 'Buy Milk',
+      description: 'I need to go to the shop and buy a gallon of milk',
+      dueDate: new Date(),
+      status: "To Do"
+    }
+  ]
 }
